041865aab8a715811f84ec002204536918f2317d
Brandon Fillpot <brandonfillpot@gmail.com>
2021-12-30 09:54:59 -0800

    async data testing

--- functions.test.js
+++ functions.test.js
@@ -22,3 +22,33 @@ test("User should be Brandon Fillpot object", () => {
     lastName: "Fillpot",
   });
 });
+
+test("should be under 1700", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1700);
+});
+
+test("should be under or equal to 1700", () => {
+  const load1 = 800;
+  const load2 = 900;
+  expect(load1 + load2).toBeLessThanOrEqual(1700);
+});
+
+test("there is no i in team", () => {
+  expect("team").not.toMatch(/I/i);
+});
+
+test("admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
+
+//async data tests
+
+test("user fetched name should be leanne graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then((data) => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
